6e7d0eab465b0fad8f28664f79e597e0f15f2011
Donato Calvillo <donato.calvillo@3mium.com>
2023-01-27 01:37:21 -0600

    feat: add page 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,4 +1,4 @@
-import { Box, Container } from '@mui/material';
+import { Box, Button, Container, Typography } from '@mui/material';
 import { UserLayout } from '../../components/layouts/UserLayout';
 import Image from 'next/image';
 import Image404 from '../../public/icons/Image404.svg'
@@ -7,10 +7,16 @@ import Image404 from '../../public/icons/Image404.svg'
 const Custom404 = () => {
   return (
     <UserLayout title='Página no encontrada' pageDescription='No se encontro lo que estabas buscando,'>
-        <Container sx={{display: 'flex'}} style={{ background: "black" }}>
-          <Box width="100%" height="100%" style={{ background: "red" }} display="flex" alignItems="center" justifyContent="center"> 
-              <Image src={ Image404 } alt="puntos" width={200}/>
-              <h1>404</h1>
+        <Container sx={{display: 'flex'}} >
+          <Box width="100%" height="90vh" display="flex" alignItems="center" justifyContent="center"> 
+              <Image src={ Image404 } alt="puntos" width={200} style={{ marginRight: "50px" }}/>
+              <Box maxWidth="300px">
+
+                <Typography variant='h2' component="h2" fontSize="60px" fontWeight="600">404</Typography>
+                <Typography variant='h3' component="h3" fontSize="22px" fontWeight="100" margin="20px 0">Ups... parece que lo que buscas ya no se encuentra aqui...</Typography>
+                <Button variant="outlined" style={{ padding:"10px 20px", fontSize: "16px" }}>Regresar al inicio</Button>
+              
+              </Box>
           </Box>
         </Container>
     </UserLayout>
