c007f81374ea3a0006b38cdc88a716dae6dab500
Donato Calvillo <donato.calvillo@3mium.com>
2023-05-13 13:56:05 -0600

    refactor: refactor code in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,56 +1,52 @@
-import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
-interface IValidateToken {
-  status: string;
+import { NextRequest, NextResponse } from "next/server";
+
+export enum ValidRoles {
+  admin = "Administrador",
+  superAdmin = "SuperAdministrador",
+  user = "Usuario",
 }
 
-export async function middleware(req: NextRequest, ev: NextFetchEvent) {
-  const currentToken = req.cookies.get("token")?.value;
+export async function middleware(req: NextRequest) {
+  const currentToken: string | undefined = req.cookies.get("token")?.value;
 
   if (!currentToken) return NextResponse.redirect(new URL("/auth", req.url));
 
-  try {
-    const data = await fetch("http://localhost:8001/api/auth/validateToken", {
-      method: "GET",
-      mode: "cors",
-      headers: {
-        Authorization: `bearer ${currentToken}`,
-      },
-    })
-      .then((data) => data.json())
-      .catch((error) => console.log(error));
+  const data = await fetch("http://localhost:8001/api/auth/validateToken", {
+    method: "GET",
+    mode: "cors",
+    headers: {
+      Authorization: `bearer ${currentToken}`,
+    },
+  })
+    .then((data) => data.json())
+    .catch((error) => NextResponse.rewrite(new URL("/", req.url)));
 
-    const { token, usuario } = data;
-    const { rol_nombre } = usuario;
+  const { usuario } = data;
+  const { rol_nombre } = usuario;
 
-    const url = req.nextUrl.clone();
-    console.log(url);
+  const url = req.nextUrl.clone();
+  console.log(url);
 
-    if (req.nextUrl.pathname.startsWith("/change-password"))
-      return NextResponse.rewrite(new URL("/change-password", req.url));
+  if (req.nextUrl.pathname.startsWith("/change-password"))
+    return NextResponse.rewrite(new URL("/change-password", req.url));
 
-    if (req.nextUrl.pathname.startsWith("/user-dashboard")) {
-      if (rol_nombre === "Usuario")
-        return NextResponse.rewrite(new URL("/user-dashboard", req.url));
-      else {
-        url.pathname = "/admin-dashboard";
-        return NextResponse.redirect(url);
-      }
-    }
+  if (req.nextUrl.pathname.startsWith("/user-dashboard")) {
+    if (rol_nombre === ValidRoles.user)
+      return NextResponse.rewrite(new URL("/user-dashboard", req.url));
 
-    if (req.nextUrl.pathname.startsWith("/admin-dashboard")) {
-      if (rol_nombre === "Administrador") return NextResponse.next();
-      // return NextResponse.rewrite(new URL('/admin-dashboard', req.url))
-      else {
-        url.pathname = "/user-dashboard";
-        return NextResponse.redirect(url);
-      }
-    }
-
-    return NextResponse.next();
-  } catch (error) {
-    console.log(error);
-    return NextResponse.redirect(new URL("/", req.url));
+    url.pathname = "/admin-dashboard";
+    return NextResponse.redirect(url);
   }
+
+  if (req.nextUrl.pathname.startsWith("/admin-dashboard")) {
+    if (rol_nombre === ValidRoles.admin || rol_nombre === ValidRoles.superAdmin)
+      return NextResponse.rewrite(new URL("/admin-dashboard", req.url));
+
+    url.pathname = "/user-dashboard";
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
