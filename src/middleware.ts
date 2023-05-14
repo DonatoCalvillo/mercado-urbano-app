aed91482489ab0e4794545d247c1b0700e3f4a6b
Donato Calvillo <donato.calvillo@3mium.com>
2023-05-13 20:16:21 -0600

    fix: router admin fixed

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,42 +11,51 @@ export async function middleware(req: NextRequest) {
 
   if (!currentToken) return NextResponse.redirect(new URL("/auth", req.url));
 
-  const data = await fetch("http://localhost:3001/api/auth/validateToken", {
-    method: "GET",
-    mode: "cors",
-    headers: {
-      Authorization: `bearer ${currentToken}`,
-    },
-  })
-    .then((data) => data.json())
-    .catch((error) => NextResponse.rewrite(new URL("/", req.url)));
+  try {
+    const data = await fetch("http://localhost:3001/api/auth/validateToken", {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        Authorization: `bearer ${currentToken}`,
+      },
+    })
+      .then((data) => data.json())
+      .catch((error) => {
+        console.log(error);
+      });
 
-  const { usuario } = data;
-  const { rol_nombre } = usuario;
+    const { usuario } = data;
+    const { rol_nombre } = usuario;
 
-  const url = req.nextUrl.clone();
-  console.log(url);
+    const url = req.nextUrl.clone();
+    console.log(rol_nombre);
+    if (req.nextUrl.pathname.startsWith("/change-password"))
+      return NextResponse.rewrite(new URL("/change-password", req.url));
 
-  if (req.nextUrl.pathname.startsWith("/change-password"))
-    return NextResponse.rewrite(new URL("/change-password", req.url));
+    if (req.nextUrl.pathname.startsWith("/user-dashboard")) {
+      if (rol_nombre === ValidRoles.user)
+        return NextResponse.rewrite(new URL("/user-dashboard", req.url));
 
-  if (req.nextUrl.pathname.startsWith("/user-dashboard")) {
-    if (rol_nombre === ValidRoles.user)
-      return NextResponse.rewrite(new URL("/user-dashboard", req.url));
+      url.pathname = "/admin-dashboard";
+      return NextResponse.redirect(url);
+    }
 
-    url.pathname = "/admin-dashboard";
-    return NextResponse.redirect(url);
+    if (req.nextUrl.pathname.startsWith("/admin-dashboard")) {
+      if (
+        rol_nombre === ValidRoles.admin ||
+        rol_nombre === ValidRoles.superAdmin
+      )
+        return NextResponse.next();
+
+      url.pathname = "/user-dashboard";
+      return NextResponse.redirect(url);
+    }
+
+    return NextResponse.next();
+  } catch (error) {
+    console.log(error);
+    return NextResponse.redirect(new URL("/", req.url));
   }
-
-  if (req.nextUrl.pathname.startsWith("/admin-dashboard")) {
-    if (rol_nombre === ValidRoles.admin || rol_nombre === ValidRoles.superAdmin)
-      return NextResponse.rewrite(new URL("/admin-dashboard", req.url));
-
-    url.pathname = "/user-dashboard";
-    return NextResponse.redirect(url);
-  }
-
-  return NextResponse.next();
 }
 
 export const config = {
