b7999876993a6b6022fd8d1413ba537f5eb08af2
Donato Calvillo <donato.calvillo@3mium.com>
2023-05-16 08:11:19 -0600

    fix: cors error

--- api/mercadoUrbanoApi.ts
+++ api/mercadoUrbanoApi.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const mercadoUrbanoApi = axios.create({
-  baseURL:
-    "https://mercado-urbano-api-2-1vrhzayo1-donatocalvillo.vercel.app/api",
+  baseURL: "https://mercado-urbano-api-2.vercel.app/api",
 });
 
 export default mercadoUrbanoApi;
